6d8cde1bc460c0c775ba32a3b6a5511996e35e85
Ann Alieksieienko <ann.alieksieienko@gmail.com>
2021-05-10 19:07:41 +0300

    Add spline interpolation to lab4

--- comp-methods/lab4/main.py
+++ comp-methods/lab4/main.py
@@ -3,6 +3,29 @@ import matplotlib.pyplot as plt
 from numpy.linalg import solve
 
 
+class EquationSolver:
+    @staticmethod
+    def jacobi(A, b, x0, tol, n_iterations=300):
+        n = A.shape[0]
+        x = x0.copy()
+        x_prev = x0.copy()
+        counter = 0
+        x_diff = tol + 1
+
+        while (x_diff > tol) and (counter < n_iterations):
+            for i in range(0, n):
+                s = 0
+                for j in range(0, n):
+                    if i != j:
+                        s += A[i, j] * x_prev[j]
+
+                x[i] = (b[i] - s) / A[i, i]
+            counter += 1
+            x_diff = (np.sum((x - x_prev) ** 2)) ** 0.5
+            x_prev = x.copy()
+        return x
+
+
 class Interpolation:
     def target_func(self, x):
         return np.sin(x)
@@ -49,6 +72,54 @@ class Interpolation:
             a[k: m] = (a[k:m] - a[k - 1]) / (x[k:m] - x[k - 1])
         return a
 
+    @staticmethod
+    def cubic_spline(x0, x, y):
+        x = np.array(x)
+        y = np.array(y)
+
+        size = len(x)
+        xdiff = np.diff(x)
+        ydiff = np.diff(y)
+        Li = np.empty(size)
+        Li_1 = np.empty(size - 1)
+        z = np.empty(size)
+
+        Li[0] = np.sqrt(2 * xdiff[0])
+        Li_1[0] = 0.0
+        B0 = 0.0
+        z[0] = B0 / Li[0]
+
+        for i in range(1, size - 1, 1):
+            Li_1[i] = xdiff[i - 1] / Li[i - 1]
+            Li[i] = np.sqrt(2 * (xdiff[i - 1] + xdiff[i]) - Li_1[i - 1] * Li_1[i - 1])
+            Bi = 6 * (ydiff[i] / xdiff[i] - ydiff[i - 1] / xdiff[i - 1])
+            z[i] = (Bi - Li_1[i - 1] * z[i - 1]) / Li[i]
+
+        i = size - 1
+        Li_1[i - 1] = xdiff[-1] / Li[i - 1]
+        Li[i] = np.sqrt(2 * xdiff[-1] - Li_1[i - 1] * Li_1[i - 1])
+        Bi = 0.0
+        z[i] = (Bi - Li_1[i - 1] * z[i - 1]) / Li[i]
+
+        i = size - 1
+        z[i] = z[i] / Li[i]
+        for i in range(size - 2, -1, -1):
+            z[i] = (z[i] - Li_1[i - 1] * z[i + 1]) / Li[i]
+
+        index = x.searchsorted(x0)
+        np.clip(index, 1, size - 1, index)
+
+        xi1, xi0 = x[index], x[index - 1]
+        yi1, yi0 = y[index], y[index - 1]
+        zi1, zi0 = z[index], z[index - 1]
+        hi1 = xi1 - xi0
+
+        f0 = zi0 / (6 * hi1) * (xi1 - x0) ** 3 + \
+             zi1 / (6 * hi1) * (x0 - xi0) ** 3 + \
+             (yi1 / hi1 - zi1 * hi1 / 6) * (x0 - xi0) + \
+             (yi0 / hi1 - zi0 * hi1 / 6) * (xi1 - x0)
+        return f0
+
 
 class Plot:
     title: str = 'Interpolation'
@@ -72,7 +143,7 @@ class Plot:
 
 if __name__ == '__main__':
     a, b = 1, 15
-    n = 50
+    n = 20
     plot = Plot()
     interpolation = Interpolation()
 
@@ -95,10 +166,12 @@ if __name__ == '__main__':
     y1 = interpolation.linear_method(x, x_init, c)
     y2 = [interpolation.lagrange(x0, x_init, y_init) for x0 in x]
     y3 = [interpolation.newton(x0, x_init, diffs) for x0 in x]
+    y4 = interpolation.cubic_spline(x, x_init, y_init)
 
     plot.add(x, y, 'r')
-    plot.add(x, y1, 'y')
+    # plot.add(x, y1, 'y')
     # plot.add(x, y2, 'g')
     # plot.add(x, y3, 'b')
+    plot.add(x, y4, 'g')
 
     plot.show()
