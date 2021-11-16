f0391ed5017254365c169c3ce64486047f7cd30d
Ann Alieksieienko <ann.alieksieienko@gmail.com>
2021-11-17 01:07:20 +0200

    lab2: Use decimal

--- comp-model/lab2/main.py
+++ comp-model/lab2/main.py
@@ -1,9 +1,10 @@
 from tabulate import tabulate
-from fractions import Fraction
+from decimal import *
 
 
 def func(x, y):
-    return 0.4 * y + 0.002 * x * (1 - 0.2 * x)
+    return Decimal('0.4') * y + \
+           Decimal('0.002') * x * (Decimal('1') - Decimal('0.2') * x)
 
 
 EPS = 10 ** (-5)
@@ -11,7 +12,7 @@ EPS = 10 ** (-5)
 
 def create_table(x0, y0, xn, h):
     x, y = x0, y0
-    x_vec, y_vec = [], []
+    x_vec, y_vec = [x0], [y0]
     while x < xn:
         x_new = x + h
         y_predicted = predict(x, y, h)
@@ -41,10 +42,10 @@ def correct(x, y, x_new, y_predicted, h):
 
 
 if __name__ == '__main__':
-    x0 = 0.1
-    y0 = 1.0408
-    xn = 1
-    h = 0.1
+    x0 = Decimal('0.1')
+    y0 = Decimal('1.0408')
+    xn = Decimal('1')
+    h = Decimal('0.1')
 
     x_vec, y_vec = create_table(x0, y0, xn, h)
     rows = [['x', 'y']]
