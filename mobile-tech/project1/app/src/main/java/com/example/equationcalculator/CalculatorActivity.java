882831764a7242ba11b84393bb82d39e2c33f754
Ann Alieksieienko <ann.alieksieienko@gmail.com>
2022-12-11 02:02:43 +0100

    Change main activity

--- mobile-tech/project1/app/src/main/java/com/example/equationcalculator/CalculatorActivity.java
+++ mobile-tech/project1/app/src/main/java/com/example/equationcalculator/CalculatorActivity.java
@@ -33,7 +33,7 @@ public class CalculatorActivity extends AppCompatActivity {
 
         setSupportActionBar(binding.toolbar);
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_calculator);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
     }
@@ -63,7 +63,7 @@ public class CalculatorActivity extends AppCompatActivity {
 
     @Override
     public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_calculator);
         return NavigationUI.navigateUp(navController, appBarConfiguration)
                 || super.onSupportNavigateUp();
     }
